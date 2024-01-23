c72db30d0b544e7bd098c47ce7c4aa1346cdf3f3
Mihkel Vaher <mihkel.vaher@industry62.com>
2024-01-23 12:47:10 +0200

    html ja css algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt="Nobe elektriauto"></img>
+      <button className="nupp" >Vajuta</button>
+    </div>
+  );
+}
+
+export default App;
