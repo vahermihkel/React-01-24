759490413c958f45a42e9cc377c94082900a6668
Mihkel Vaher <mihkel.vaher@industry62.com>
2024-01-25 12:40:49 +0200

    index.js

--- veebipood/src/index.js
+++ veebipood/src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+
+// Navigeerimiseks (URL muutmine --> HTML muutmine sellega üheskoos)
+// 1. Pean katkesama rakenduse töö ja kirjutama uue mooduli pealepanemiseks:
+//      npm install react-router-dom
+// 2. importima node_modules kaustast BrowserRouteri
+// 3. Ümbritsema <App /> elemendi (tagi) BrowserRouteriga
+// 4. App.js failis pean tekitama URLi ja HTMLi seoseid
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
