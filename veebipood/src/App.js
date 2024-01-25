d17849c5f65be5dfcc00c12e7c2e7ba99df1029d
Mihkel Vaher <mihkel.vaher@industry62.com>
2024-01-25 11:19:55 +0200

    html ja css

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <img className="pilt" src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt="Nobe elektriauto"></img>
-      <button className="nupp" >Vajuta</button>
+      <button className="nupp">Vajuta</button>
+      <span>Tekst</span>
+      <div >Tekst2</div>
+      <h3>Pealkiri</h3>
     </div>
   );
 }
